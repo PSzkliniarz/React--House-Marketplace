075829d15cdc4b934a0975de9ffff87f7678b244
Pawel Szkliniarz <Pawel.Szkliniarz@pl.ey.com>
2022-06-21 13:06:32 +0200

    Added user authentication and save users in users database table

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
-    neme: '',
+    name: '',
     email: '',
     password: '',
   })
@@ -22,7 +25,28 @@ function SignUp() {
   }
 
   const onSubmit = async (e) => {
+    e.preventDefault()
 
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile( auth.currentUser, { displayName: name})
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+
+    } catch (error) {
+      console.log(error);      
+    }
   }
 
   return (
@@ -33,14 +57,6 @@ function SignUp() {
         </header>
 
         <form onSubmit={onSubmit}>
-          <input
-            type='email'
-            className='emailInput'
-            placeholder='Email'
-            id='email'
-            value={email}
-            onChange={onChange}
-          />
 
           <input
             type='text'
@@ -48,6 +64,15 @@ function SignUp() {
             placeholder='Name'
             id='name'
             value={name}
+            onChange={onChange}
+          />
+
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
             onChange={onChange}
           />
 
