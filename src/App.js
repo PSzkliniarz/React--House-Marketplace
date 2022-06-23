9471e2b397a058a21fe8aa1151968a1f7486b0e3
Pawel Szkliniarz <Pawel.Szkliniarz@pl.ey.com>
2022-06-23 16:28:48 +0200

    Private route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
+import PrivateRoute from './components/PrivateRoute';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Navbar from './components/Navbar';
@@ -17,7 +18,9 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
         </Routes>
