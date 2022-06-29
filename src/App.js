e65131e77c47d023e253c7ba453262ac15f3dc23
Pawel Szkliniarz <Pawel.Szkliniarz@pl.ey.com>
2022-06-29 17:12:35 +0200

    Fetching listings from DB

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
+import Category from './pages/Category';
 import Profile from './pages/Profile';
 import PrivateRoute from './components/PrivateRoute';
 import SignIn from './pages/SignIn';
@@ -18,6 +19,7 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
